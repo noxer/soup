51c3d26d0e1add8a06a5ba804e84ab8ad6534198
Tim Scheuermann <tim@plusmid.net>
2018-09-01 14:49:47 +0200

    Removed panics, added size checks, rewrote methods and fixed a few bugs

--- soup.go
+++ soup.go
@@ -6,9 +6,9 @@ package soup
 
 import (
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
-	"regexp"
 	"strings"
 
 	"golang.org/x/net/html"
@@ -21,7 +21,22 @@ type Root struct {
 	Error     error
 }
 
-var debug = false
+func wrapNode(n *html.Node) Root {
+
+	return Root{
+		Pointer:   n,
+		NodeValue: n.Data,
+	}
+
+}
+
+func wrapErr(err error) Root {
+	return Root{Error: err}
+}
+
+func wrapErrf(format string, a ...interface{}) Root {
+	return wrapErr(fmt.Errorf(format, a...))
+}
 
 // Headers contains all HTTP headers to send
 var Headers = make(map[string]string)
@@ -29,77 +44,65 @@ var Headers = make(map[string]string)
 // Cookies contains all HTTP cookies to send
 var Cookies = make(map[string]string)
 
-// SetDebug sets the debug status
-// Setting this to true causes the panics to be thrown and logged onto the console.
-// Setting this to false causes the errors to be saved in the Error field in the returned struct.
-func SetDebug(d bool) {
-	debug = d
-}
-
 // Header sets a new HTTP header
 func Header(n string, v string) {
 	Headers[n] = v
 }
 
+// Cookie sets a cookie to send
 func Cookie(n string, v string) {
 	Cookies[n] = v
 }
 
 // GetWithClient returns the HTML returned by the url using a provided HTTP client
 func GetWithClient(url string, client *http.Client) (string, error) {
+
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		if debug {
-			panic("Couldn't perform GET request to " + url)
-		}
 		return "", errors.New("couldn't perform GET request to " + url)
 	}
+
 	// Set headers
-	for hName, hValue := range Headers {
-		req.Header.Set(hName, hValue)
+	for name, val := range Headers {
+		req.Header.Set(name, val)
 	}
+
 	// Set cookies
-	for cName, cValue := range Cookies {
+	for name, val := range Cookies {
 		req.AddCookie(&http.Cookie{
-			Name:  cName,
-			Value: cValue,
+			Name:  name,
+			Value: val,
 		})
 	}
+
 	// Perform request
 	resp, err := client.Do(req)
 	if err != nil {
-		if debug {
-			panic("Couldn't perform GET request to " + url)
-		}
 		return "", errors.New("couldn't perform GET request to " + url)
 	}
 	defer resp.Body.Close()
+
 	bytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		if debug {
-			panic("Unable to read the response body")
-		}
 		return "", errors.New("unable to read the response body")
 	}
+
 	return string(bytes), nil
 }
 
 // Get returns the HTML returned by the url in string using the default HTTP client
 func Get(url string) (string, error) {
-	// Init a new HTTP client
-	client := &http.Client{}
-	return GetWithClient(url, client)
+	return GetWithClient(url, http.DefaultClient)
 }
 
 // HTMLParse parses the HTML returning a start pointer to the DOM
 func HTMLParse(s string) Root {
+
 	r, err := html.Parse(strings.NewReader(s))
 	if err != nil {
-		if debug {
-			panic("Unable to parse the HTML")
-		}
-		return Root{nil, "", errors.New("unable to parse the HTML")}
+		return wrapErrf("unable to parse the HTML")
 	}
+
 	for r.Type != html.ElementNode {
 		switch r.Type {
 		case html.DocumentNode:
@@ -110,21 +113,27 @@ func HTMLParse(s string) Root {
 			r = r.NextSibling
 		}
 	}
-	return Root{r, r.Data, nil}
+
+	return wrapNode(r)
+
 }
 
 // Find finds the first occurrence of the given tag name,
 // with or without attribute key and value specified,
 // and returns a struct with a pointer to it
 func (r Root) Find(args ...string) Root {
-	temp, ok := findOnce(r.Pointer, args, false, false)
-	if ok == false {
-		if debug {
-			panic("Element `" + args[0] + "` with attributes `" + strings.Join(args[1:], " ") + "` not found")
-		}
-		return Root{nil, "", errors.New("element `" + args[0] + "` with attributes `" + strings.Join(args[1:], " ") + "` not found")}
+
+	if len(args) == 0 {
+		return wrapErrf("not enough arguments")
 	}
-	return Root{temp, temp.Data, nil}
+
+	temp, ok := findOnce(r.Pointer, args, false, false)
+	if !ok {
+		return wrapErrf("element `%s` with attributes `%s` not found", args[0], strings.Join(args[1:], " "))
+	}
+
+	return wrapNode(temp)
+
 }
 
 // FindAll finds all occurrences of the given tag name,
@@ -132,164 +141,179 @@ func (r Root) Find(args ...string) Root {
 // and returns an array of structs, each having
 // the respective pointers
 func (r Root) FindAll(args ...string) []Root {
+
 	temp := findAllofem(r.Pointer, args, false)
 	if len(temp) == 0 {
-		if debug {
-			panic("Element `" + args[0] + "` with attributes `" + strings.Join(args[1:], " ") + "` not found")
-		}
-		return []Root{}
+		return nil
 	}
-	pointers := make([]Root, 0, len(temp))
-	for i := 0; i < len(temp); i++ {
-		pointers = append(pointers, Root{temp[i], temp[i].Data, nil})
+
+	ps := make([]Root, len(temp))
+	for i, t := range temp {
+		ps[i] = wrapNode(t)
 	}
-	return pointers
+
+	return ps
 }
 
 // FindStrict finds the first occurrence of the given tag name
 // only if all the values of the provided attribute are an exact match
 func (r Root) FindStrict(args ...string) Root {
-	temp, ok := findOnce(r.Pointer, args, false, true)
-	if ok == false {
-		if debug {
-			panic("Element `" + args[0] + "` with attributes `" + strings.Join(args[1:], " ") + "` not found")
-		}
-		return Root{nil, "", errors.New("element `" + args[0] + "` with attributes `" + strings.Join(args[1:], " ") + "` not found")}
+
+	if len(args) == 0 {
+		return wrapErrf("not enough arguments")
 	}
-	return Root{temp, temp.Data, nil}
+
+	temp, ok := findOnce(r.Pointer, args, false, true)
+	if !ok {
+		return wrapErrf("element `%s` with attributes `%s` not found", args[0], strings.Join(args[1:], " "))
+	}
+
+	return wrapNode(temp)
+
 }
 
 // FindAllStrict finds all occurrences of the given tag name
 // only if all the values of the provided attribute are an exact match
 func (r Root) FindAllStrict(args ...string) []Root {
+
 	temp := findAllofem(r.Pointer, args, true)
 	if len(temp) == 0 {
-		if debug {
-			panic("Element `" + args[0] + "` with attributes `" + strings.Join(args[1:], " ") + "` not found")
-		}
-		return []Root{}
+		return nil
 	}
-	pointers := make([]Root, 0, len(temp))
-	for i := 0; i < len(temp); i++ {
-		pointers = append(pointers, Root{temp[i], temp[i].Data, nil})
+
+	ps := make([]Root, len(temp))
+	for i, t := range temp {
+		ps[i] = wrapNode(t)
 	}
-	return pointers
+
+	return ps
+
 }
 
 // FindNextSibling finds the next sibling of the pointer in the DOM
 // returning a struct with a pointer to it
 func (r Root) FindNextSibling() Root {
-	nextSibling := r.Pointer.NextSibling
-	if nextSibling == nil {
-		if debug {
-			panic("No next sibling found")
-		}
-		return Root{nil, "", errors.New("no next sibling found")}
+
+	next := r.Pointer.NextSibling
+	if next == nil {
+		return wrapErrf("no next sibling found")
 	}
-	return Root{nextSibling, nextSibling.Data, nil}
+	return wrapNode(next)
+
 }
 
 // FindPrevSibling finds the previous sibling of the pointer in the DOM
 // returning a struct with a pointer to it
 func (r Root) FindPrevSibling() Root {
-	prevSibling := r.Pointer.PrevSibling
-	if prevSibling == nil {
-		if debug {
-			panic("No previous sibling found")
-		}
-		return Root{nil, "", errors.New("no previous sibling found")}
+
+	prev := r.Pointer.PrevSibling
+	if prev == nil {
+		return wrapErrf("no previous sibling found")
 	}
-	return Root{prevSibling, prevSibling.Data, nil}
+	return wrapNode(prev)
 }
 
 // FindNextElementSibling finds the next element sibling of the pointer in the DOM
 // returning a struct with a pointer to it
 func (r Root) FindNextElementSibling() Root {
-	nextSibling := r.Pointer.NextSibling
-	if nextSibling == nil {
-		if debug {
-			panic("No next element sibling found")
+
+	for k := r.Pointer.NextSibling; k != nil; k = k.NextSibling {
+
+		if k.Type == html.ElementNode {
+			return wrapNode(k)
 		}
-		return Root{nil, "", errors.New("no next element sibling found")}
+
 	}
-	if nextSibling.Type == html.ElementNode {
-		return Root{nextSibling, nextSibling.Data, nil}
-	}
-	p := Root{nextSibling, nextSibling.Data, nil}
-	return p.FindNextElementSibling()
+
+	return wrapErrf("no next element sibling found")
+
 }
 
 // FindPrevElementSibling finds the previous element sibling of the pointer in the DOM
 // returning a struct with a pointer to it
 func (r Root) FindPrevElementSibling() Root {
-	prevSibling := r.Pointer.PrevSibling
-	if prevSibling == nil {
-		if debug {
-			panic("No previous element sibling found")
+
+	for k := r.Pointer.PrevSibling; k != nil; k = k.PrevSibling {
+
+		if k.Type == html.ElementNode {
+			return wrapNode(k)
 		}
-		return Root{nil, "", errors.New("no previous element sibling found")}
+
 	}
-	if prevSibling.Type == html.ElementNode {
-		return Root{prevSibling, prevSibling.Data, nil}
-	}
-	p := Root{prevSibling, prevSibling.Data, nil}
-	return p.FindPrevElementSibling()
+
+	return wrapErrf("no previous element sibling found")
+
 }
 
 // Children retuns all direct children of this DOME element.
 func (r Root) Children() []Root {
-	child := r.Pointer.FirstChild
-	var children []Root
-	for child != nil {
-		children = append(children, Root{child, child.Data, nil})
-		child = child.NextSibling
+
+	var cs []Root
+
+	for k := r.Pointer.FirstChild; k != nil; k = k.NextSibling {
+		cs = append(cs, Root{k, k.Data, nil})
 	}
-	return children
+
+	return cs
+
 }
 
 // Attrs returns a map containing all attributes
 func (r Root) Attrs() map[string]string {
+
 	if r.Pointer.Type != html.ElementNode {
-		if debug {
-			panic("Not an ElementNode")
-		}
 		return nil
 	}
-	if len(r.Pointer.Attr) == 0 {
-		return nil
-	}
+
 	return getKeyValue(r.Pointer.Attr)
+
 }
 
 // Text returns the string inside a non-nested element
 func (r Root) Text() string {
-	k := r.Pointer.FirstChild
-checkNode:
-	if k != nil && k.Type != html.TextNode {
-		k = k.NextSibling
-		if k == nil {
-			if debug {
-				panic("No text node found")
-			}
-			return ""
-		}
-		goto checkNode
+
+	// Fast path, the element itself is a `html.TextNode`
+	if r.Pointer.Type == html.TextNode {
+		return r.Pointer.Data
 	}
-	if k != nil {
-		r, _ := regexp.Compile(`^\s+$`)
-		if ok := r.MatchString(k.Data); ok {
-			k = k.NextSibling
-			if k == nil {
-				if debug {
-					panic("No text node found")
-				}
-				return ""
-			}
-			goto checkNode
+
+	s := &strings.Builder{}
+
+	for k := r.Pointer.FirstChild; k != nil; k = k.NextSibling {
+
+		if k.Type == html.TextNode {
+			s.WriteString(k.Data)
 		}
-		return k.Data
+
 	}
-	return ""
+
+	return s.String()
+
+}
+
+// FullText combines all the text elements withing this element,
+// including the nested elements.
+func (r Root) FullText() string {
+
+	s := &strings.Builder{}
+	fullText(s, r.Pointer)
+	return s.String()
+
+}
+
+func fullText(s *strings.Builder, n *html.Node) {
+
+	if n.Type == html.TextNode {
+
+		s.WriteString(n.Data)
+		return
+
+	}
+
+	for k := n.FirstChild; k != nil; k = k.NextSibling {
+		fullText(s, k)
+	}
+
 }
 
 // Using depth first search to find the first occurrence and return
@@ -323,6 +347,7 @@ func findOnce(n *html.Node, args []string, uni bool, strict bool) (*html.Node, b
 
 // Using depth first search to find all occurrences and return
 func findAllofem(n *html.Node, args []string, strict bool) []*html.Node {
+
 	var nodeLinks = make([]*html.Node, 0, 10)
 	var f func(*html.Node, []string, bool)
 	f = func(n *html.Node, args []string, uni bool) {
@@ -354,31 +379,38 @@ func findAllofem(n *html.Node, args []string, strict bool) []*html.Node {
 
 // attributeAndValueEquals reports when the html.Attribute attr has the same attribute name and value as from
 // provided arguments
-func attributeAndValueEquals(attr html.Attribute, attribute, value string) bool {
-	return attr.Key == attribute && attr.Val == value
+func attributeAndValueEquals(attr html.Attribute, key, val string) bool {
+	return attr.Key == key && attr.Val == val
 }
 
 // attributeContainsValue reports when the html.Attribute attr has the same attribute name as from provided
 // attribute argument and compares if it has the same value in its values parameter
-func attributeContainsValue(attr html.Attribute, attribute, value string) bool {
-	if attr.Key == attribute {
-		for _, attrVal := range strings.Fields(attr.Val) {
-			if attrVal == value {
-				return true
-			}
+func attributeContainsValue(attr html.Attribute, key, val string) bool {
+
+	if attr.Key != key {
+		return false
+	}
+
+	fs := strings.Fields(attr.Val)
+	for _, f := range fs {
+		if f == val {
+			return true
 		}
 	}
+
 	return false
+
 }
 
 // Returns a key pair value (like a dictionary) for each attribute
-func getKeyValue(attributes []html.Attribute) map[string]string {
-	var keyvalues = make(map[string]string)
-	for i := 0; i < len(attributes); i++ {
-		_, exists := keyvalues[attributes[i].Key]
-		if exists == false {
-			keyvalues[attributes[i].Key] = attributes[i].Val
-		}
+func getKeyValue(attrs []html.Attribute) map[string]string {
+
+	kv := make(map[string]string, len(attrs))
+
+	for _, attr := range attrs {
+		kv[attr.Key] = attr.Val
 	}
-	return keyvalues
+
+	return kv
+
 }
